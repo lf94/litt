5483db169d906193a1e25021675572f0c8990369
lf94 <ircsurfer33@gmail.com>
2017-01-16 22:52:54 -0500

    Remove spaces

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-module.exports = (a = {}) => ({
- on(t, x) {
-  a[t] ? a[t].push(x) : a[t] = [x]
- },
- off(t, x) {
-  a[t] = a[t].filter(f => f != x)
- },
- emit(t, x) {
-  a[t].map(f => f(x))
- },
-})
+e = (a = {}) => ({ on(t, x) { a[t] ? a[t].push(x) : a[t] = [x] }, off(t, x) { a[t] = a[t].filter(f => f != x) }, emit(t, x) { a[t].map(f => f(x)) }, })
