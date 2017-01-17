3b520052d3f0076ff3227d88534e02b3b65e7031
lf94 <ircsurfer33@gmail.com>
2017-01-16 22:07:22 -0500

    Probably the first and only commit ever to this repo

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = (a = {}) => ({
+ on(t, x) {
+  a[t] ? a[t].push(x) : a[t] = [x]
+ },
+ off(t, x) {
+  a[t] = a[t].filter(f => f != x)
+ },
+ emit(t, x) {
+  a[t].map(f => f(x))
+ },
+})
